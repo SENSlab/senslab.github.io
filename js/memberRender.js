bf88bc318e2afd288546e49a7cf31366612667f1
narugit <naruiroiro@gmail.com>
2018-06-01 18:08:33 +0900

    Add memberRender.js and modify people.html for easier management of members

---
+++ js/memberRender.js
@@ -0,0 +1,93 @@
+//After render the screen
+$(function() {
+  //Get data from data/news.json and put it in the json object
+  $.getJSON("data/member.json", function(json){
+    json.forEach(function( members ) {
+      var countByFour = 0;
+      memberInfo = "";
+      attribute =
+        "<div class=\"row\">" +
+        "</br>" +
+        "</br>" +
+        "<h3 class=\"headline\">" +
+          members.attribute +
+        "</h3>" +
+        "<hr>";
+      memberInfo += attribute;
+
+      members.people.forEach(function( member ){
+        if(countByFour == 4){
+          row =
+          "</div>"+
+          "<div class=\"row\">" +
+          "</br>" +
+          "</br>" +
+          "<hr>";
+          countByFour = 0;
+        }
+        else{
+          row = "";
+        }
+
+        /** one member needs "photo", "nameJP",
+            "nameEN", "position", and "positionOther"**/
+        photo =
+          "<div class=\"col-md-3\" style=\"text-align: center;\">" +
+          "<img src=\"" +
+          member.photoPass +
+          "\" class=\"img-circle\" alt=\"" +
+          member.nameEN +
+          "\" width=\"130\" height=\"130\">" +
+          "<br />";
+
+        nameJP =
+          "<span class=\"current-lab-member-name\">" +
+          member.nameJP +
+          "<br />";
+
+        nameEN =
+          member.nameEN +
+          "</span>" +
+          "<br />";
+
+        if(member.position != ""){
+          position =
+            "<span class=\"current-lab-member-position\">" +
+            member.position;
+        }
+        else{
+          position ="<span>";
+        }
+
+        if(member.positionOther != ""){
+          positionOther =
+            "<br />" +
+            member.positionOther +
+            "</span>" +
+    		    "</div>";
+        }
+        else{
+          positionOther =
+          "</span>" +
+          "</div>";
+        }
+
+        countByFour++;
+
+        memberInfo += row + photo + nameJP + nameEN + position + positionOther;
+      });
+      emptyMemberForStyle = "";
+
+      for(i = 0; i < 4 - countByFour; i++){
+        emptyMemberForStyle +=
+        "<div class=\"col-md-3\" style=\"text-align: center;\">" +
+        "</div>";
+      }
+
+      divEnd = "</div>";
+
+      memberInfo += emptyMemberForStyle + divEnd;
+      $("#member").append(memberInfo);
+    });
+  });
+});
