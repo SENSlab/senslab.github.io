59dcef85bd371cf088e12d2d693104df9f8d7f46
narugit <narusens@gmail.com>
2018-08-09 22:56:12 +0900

    #4: Fix icon and font size.

--- js/historyRender.js
+++ js/historyRender.js
@@ -4,39 +4,39 @@ $(function() {
   $.getJSON("data/history.json", function(json){
       json.forEach(function( history ){
         head =
-        "<div class=\"cd-timeline__block js-cd-block\">";
+        "<div class='cd-timeline__block js-cd-block'>";
 
           if(history.genre === "sigma"){
             img =
-            "<div class=\"cd-timeline__img cd-timeline__img--sigma-background js-cd-img\">" +
-        			"<img src=\"images/icons/sigma-history-icon.svg\" alt=\"Location\">" +
+            "<div class='cd-timeline__img cd-timeline__img--sigma-background js-cd-img'>" +
+        			"<img src='images/icons/sigma-history-icon.svg' alt='Location'>" +
             "</div>";
           }
           else if(history.genre === "sens"){
             img =
-            "<div class=\"cd-timeline__img cd-timeline__img--sens-background js-cd-img\">" +
-        			"<img src=\"images/icons/sens-history-icon.svg\" alt=\"Location\">" +
+            "<div class='cd-timeline__img cd-timeline__img--sens-background js-cd-img'>" +
+        			"<img src='images/icons/sens-history-icon.svg' alt='Location'>" +
             "</div>";
           }
 
           topic =
-          "<div class=\"cd-timeline__content js-cd-content\">" +
-            "<h2 class=\"history-h2\">" +
-            history.topic +
-            "</h2>";
+          "<div class='cd-timeline__content js-cd-content'>" +
+            "<div class='history-topic'>" +
+                history.topic +
+            "</div>";
 
             if(history.content !== undefined){
               content =
-              "<p>" +
+              "<div>" +
                 history.content +
-              "</p>";
+              "</div>";
             }
             else{
               content = "";
             }
 
             date =
-            "<span class=\"cd-timeline__date\">" +
+            "<span class='cd-timeline__date'>" +
               history.date +
             "</span>";
 
