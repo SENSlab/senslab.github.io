87b1cd783761231e2c1966cad2f3f4a8e2fc5244
kamino410 <kamino.dev@gmail.com>
2018-08-19 21:38:21 +0900

    modified 'home.component.html' to correspond to angular

---
+++ src/js/homeBxslider.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.bxslider').bxSlider({
+        auto: true, //自動スライド / auto slide
+        pause: 4500, //待ち時間 / pauseing time
+        speed: 2200  //スピード / speed
+    });
+});
