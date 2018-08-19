e423871655648028f5156b5ec2f7e8e76208a606
kamino410 <kamino.dev@gmail.com>
2018-08-19 23:55:31 +0900

    modified bxslider.js to correspond to SPA

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+declare var startSlider;
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('bxslider')
+  bxslider: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    startSlider();
   }
-
 }
