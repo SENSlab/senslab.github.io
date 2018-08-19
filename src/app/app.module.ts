5fa7ee6f3ec4dc47a490b2b416ab9aa2941357e3
kamino410 <kamino.dev@gmail.com>
2018-08-19 21:41:32 +0900

    created research component / moved research.html into the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { ResearchComponent } from './research/research.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ResearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/research/research.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-research',
+  templateUrl: './research.component.html',
+  styleUrls: ['./research.component.css']
+})
+export class ResearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
