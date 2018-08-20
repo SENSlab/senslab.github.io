2ac214ddade3fe025519d30227ea883f585be6a5
kamino410 <kamino.dev@gmail.com>
2018-08-20 11:58:08 +0900

    adjust classes component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ResearchComponent } from './research/research.component';
 import { ContactusComponent } from './contactus/contactus.component';
+import { ClassesComponent } from './classes/classes.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'research', component: ResearchComponent },
   { path: 'contact', component: ContactusComponent },
+  { path: 'classes', component: ClassesComponent },
 ];
 
 @NgModule({
