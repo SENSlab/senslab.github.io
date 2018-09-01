24cfb2b46b1707bd56d5ba23e4acdde54dcea1f1
kamino410 <kamino.dev@gmail.com>
2018-09-01 19:24:38 +0900

    replaced jQuery+Bootstrap to MDBootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,6 +33,7 @@ import { HistoryComponent } from './history/history.component';
   ],
   imports: [
     BrowserModule,
+    MDBBootstrapModule.forRoot(),
     AppRoutingModule,
     HttpClientModule
   ],
